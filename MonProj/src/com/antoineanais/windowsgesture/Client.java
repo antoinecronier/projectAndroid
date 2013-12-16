2273b50909e13e300d35516448ed55adac5a4ea9
antoine cronier <antoinecronier@live.fr>
2013-12-16 16:51:16 +0100

    Start use dataprovider for bdd accessing

--- MonProj/src/com/antoineanais/windowsgesture/Client.java
+++ MonProj/src/com/antoineanais/windowsgesture/Client.java
@@ -1,28 +1,131 @@
 package com.antoineanais.windowsgesture;
 
+import java.util.List;
+
+import android.content.ContentValues;
+import android.content.Context;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
+
 public class Client {
-	
-	/*Members*/
+
+	/* Members */
 	private int id_client;
 	private String nom;
-	
-	/*constructor*/
+
+	/* constructor */
 	public Client(String nom) {
 		super();
 		this.nom = nom;
 	}
+
+	public Client(Context context, int id){
+		super();
+		this.id_client = id;
+		getClientID(context, id);
+	}
 	
-	/*Getters & setters*/
+	/* Getters & setters */
 	public int getId_client() {
 		return id_client;
 	}
+
 	public void setId_client(int id_client) {
 		this.id_client = id_client;
 	}
+
 	public String getNom() {
 		return nom;
 	}
+
 	public void setNom(String nom) {
 		this.nom = nom;
+	}
+
+	public void insertClient(Context context) {
+		dataprovider data = new dataprovider(context);
+		SQLiteDatabase db = data.getWritableDatabase();
+
+		String[] COL = { data.CLIENT_NOM };
+		String[] WHERE = {};
+
+		ContentValues content = new ContentValues();
+
+		content.put(data.CLIENT_NOM, getNom());
+
+		setId_client(db.insert(data.TABLE_NAME_CLIENT, null, content));
+		
+		db.close();
+	}
+
+	public void getClientID(Context context, int ID) {
+		dataprovider data = new dataprovider(context);
+		SQLiteDatabase db = data.getReadableDatabase();
+
+		Cursor monCu;
+
+		String[] COL = { data.CLIENT_ID, data.CLIENT_NOM };
+		String WHERE = data.CLIENT_ID + " = ?";
+		String[] CLAUSE = { String.valueOf(getId_client()) };
+
+		monCu = db.query(data.TABLE_NAME_CLIENT, COL, WHERE, CLAUSE, null,
+				null, null);
+		if (monCu.moveToFirst()) {
+			do {
+				setNom(monCu.getString(1));
+			} while (monCu.moveToNext());
+		}
+		db.close();
+	}
+
+	public void getClientNom(Context context) {
+		dataprovider data = new dataprovider(context);
+		SQLiteDatabase db = data.getReadableDatabase();
+
+		Cursor monCu;
+
+		String[] COL = { data.CLIENT_ID, data.CLIENT_NOM };
+		String WHERE = data.CLIENT_NOM + " = ?";
+		String[] CLAUSE = { getNom() };
+
+		monCu = db.query(data.TABLE_NAME_CLIENT, COL, WHERE, CLAUSE, null,
+				null, null);
+		if (monCu.moveToFirst()) {
+			do {
+				setId_client(monCu.getInt(0));
+			} while (monCu.moveToNext());
+		}
+		db.close();
+	}
+	
+	public int upDateClient(Context context) {
+		dataprovider data = new dataprovider(context);
+		SQLiteDatabase db = data.getWritableDatabase();
+
+		int toReturn = 0;
+		String[] COL = { data.CLIENT_ID, data.CLIENT_NOM };
+		String[] WHERE = {};
+
+		ContentValues content = new ContentValues();
+
+		content.put(data.CLIENT_ID, getId_client());
+		content.put(data.CLIENT_NOM, getNom());
+
+		toReturn += db.update(data.TABLE_NAME_CLIENT, content, data.CLIENT_ID
+				+ " = ?", new String[] { String.valueOf(getId_client()) });
+
+		db.close();
+
+		return toReturn;
+	}
+
+	public void deletedClient(Context context) {
+		dataprovider data = new dataprovider(context);
+		SQLiteDatabase db = data.getReadableDatabase();
+
+		db.delete(data.TABLE_NAME_CLIENT, data.CLIENT_ID + " = ?",
+				new String[] { getId_client() });
+
+		db.close();
 	}
 }
--- MonProj/src/com/antoineanais/windowsgesture/Commande.java
+++ MonProj/src/com/antoineanais/windowsgesture/Commande.java
@@ -2,22 +2,28 @@ package com.antoineanais.windowsgesture;
 
 import java.util.ArrayList;
 
+import android.content.ContentValues;
+import android.content.Context;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
+
 public class Commande {
 
-	/*Members*/
+	/* Members */
 	private int id_cmd;
-	private int id_client;
+	private Client client;
 	private String dateCreation;
 	private String dateFin;
 	private String dateLivraison;
 	private int avancement;
 	private ArrayList<Produit> produits;
-	
-	/*constructor*/
+
+	/* constructor */
 	public Commande(int id_client, String dateCreation, String dateFin,
-			String dateLivraison, int avancement, ArrayList<Produit> produits) {
+			String dateLivraison, int avancement, ArrayList<Produit> produits,
+			Context context) {
 		super();
-		this.id_client = id_client;
+		this.client = new Client(context, id_client);
 		this.dateCreation = dateCreation;
 		this.dateFin = dateFin;
 		this.dateLivraison = dateLivraison;
@@ -25,47 +31,153 @@ public class Commande {
 		this.produits = produits;
 	}
 
-	/*Getters & setters*/
+	/* Getters & setters */
 	public int getId_cmd() {
 		return id_cmd;
 	}
+
 	public void setId_cmd(int id_cmd) {
 		this.id_cmd = id_cmd;
 	}
-	public int getId_client() {
-		return id_client;
+
+	public Client getClient() {
+		return client;
 	}
-	public void setId_client(int id_client) {
-		this.id_client = id_client;
+
+	public void setClient(Client client) {
+		this.client = client;
 	}
+
 	public String getDateCreation() {
 		return dateCreation;
 	}
+
 	public void setDateCreation(String dateCreation) {
 		this.dateCreation = dateCreation;
 	}
+
 	public String getDateFin() {
 		return dateFin;
 	}
+
 	public void setDateFin(String dateFin) {
 		this.dateFin = dateFin;
 	}
+
 	public String getDateLivraison() {
 		return dateLivraison;
 	}
+
 	public void setDateLivraison(String dateLivraison) {
 		this.dateLivraison = dateLivraison;
 	}
+
 	public int getAvancement() {
 		return avancement;
 	}
+
 	public void setAvancement(int avancement) {
 		this.avancement = avancement;
 	}
+
 	public ArrayList<Produit> getProduits() {
 		return produits;
 	}
+
 	public void setProduits(ArrayList<Produit> produits) {
 		this.produits = produits;
+	}
+
+	public void insertCommand(Context context) {
+		dataprovider data = new dataprovider(context);
+		SQLiteDatabase db = data.getWritableDatabase();
+
+		String[] COL = { data.COMMANDE_ClIENT_ID, data.COMMANDE_DATECREATION,
+				data.COMMANDE_DATEFIN, data.COMMANDE_DATELIVRAISON,
+				data.COMMANDE_AVANCEMENT };
+		String[] WHERE = {};
+
+		ContentValues content = new ContentValues();
+
+		content.put(data.COMMANDE_ClIENT_ID, getClient().getId_client());
+		content.put(data.COMMANDE_DATECREATION, getDateCreation());
+		content.put(data.COMMANDE_DATEFIN, getDateFin());
+		content.put(data.COMMANDE_DATELIVRAISON, getDateLivraison());
+		content.put(data.COMMANDE_AVANCEMENT, getAvancement());
+
+		setId_cmd(db.insert(data.TABLE_NAME_COMMANDE, null, content));
+
+		db.close();
+	}
+
+	public void getClientID(Context context, int ID) {
+		dataprovider data = new dataprovider(context);
+		SQLiteDatabase db = data.getReadableDatabase();
+
+		Cursor monCu;
+
+		String[] COL = { data.CLIENT_ID, data.CLIENT_NOM };
+		String WHERE = data.CLIENT_ID + " = ?";
+		String[] CLAUSE = { String.valueOf(getId_client()) };
+
+		monCu = db.query(data.TABLE_NAME_CLIENT, COL, WHERE, CLAUSE, null,
+				null, null);
+		if (monCu.moveToFirst()) {
+			do {
+				setNom(monCu.getString(1));
+			} while (monCu.moveToNext());
+		}
+		db.close();
+	}
+
+	public void getClientNom(Context context) {
+		dataprovider data = new dataprovider(context);
+		SQLiteDatabase db = data.getReadableDatabase();
+
+		Cursor monCu;
+
+		String[] COL = { data.CLIENT_ID, data.CLIENT_NOM };
+		String WHERE = data.CLIENT_NOM + " = ?";
+		String[] CLAUSE = { getNom() };
+
+		monCu = db.query(data.TABLE_NAME_CLIENT, COL, WHERE, CLAUSE, null,
+				null, null);
+		if (monCu.moveToFirst()) {
+			do {
+				setId_client(monCu.getInt(0));
+			} while (monCu.moveToNext());
+		}
+		db.close();
+	}
+
+	public int upDateClient(Context context) {
+		dataprovider data = new dataprovider(context);
+		SQLiteDatabase db = data.getWritableDatabase();
+
+		int toReturn = 0;
+		String[] COL = { data.CLIENT_ID, data.CLIENT_NOM };
+		String[] WHERE = {};
+
+		ContentValues content = new ContentValues();
+
+		content.put(data.CLIENT_ID, getId_client());
+		content.put(data.CLIENT_NOM, getNom());
+
+		toReturn += db.update(data.TABLE_NAME_CLIENT, content, data.CLIENT_ID
+				+ " = ?", new String[] { String.valueOf(getId_client()) });
+
+		db.close();
+
+		return toReturn;
+	}
+
+	public void deletedClient(Context context) {
+		dataprovider data = new dataprovider(context);
+		SQLiteDatabase db = data.getReadableDatabase();
+
+		db.delete(data.TABLE_NAME_CLIENT, data.CLIENT_ID + " = ?",
+				new String[] { getId_client() });
+
+		db.close();
 	}
 }
