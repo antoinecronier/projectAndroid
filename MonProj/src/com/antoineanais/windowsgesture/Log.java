032a7a68ecd4b5351aa8d2496d0415b35f5abaae
antoine cronier <antoinecronier@live.fr>
2013-12-17 11:37:25 +0100

    progress on database using

--- MonProj/src/com/antoineanais/windowsgesture/Log.java
+++ MonProj/src/com/antoineanais/windowsgesture/Log.java
@@ -1,10 +1,17 @@
 package com.antoineanais.windowsgesture;
 
+import javax.crypto.Mac;
+
+import android.content.ContentValues;
+import android.content.Context;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
+
 public class Log {
 	/* Members */
 	private int id_log;
-	private int id_produit;
-	private int id_machine;
+	private Produit produit;
+	private Machine machine;
 	private String duree;
 	private String dateEntre;
 	private String dateSortie;
@@ -13,44 +20,167 @@ public class Log {
 	public int getId_log() {
 		return id_log;
 	}
+
 	public void setId_log(int id_log) {
 		this.id_log = id_log;
 	}
-	public int getId_produit() {
-		return id_produit;
+
+	public Produit get_produit() {
+		return produit;
 	}
-	public void setId_produit(int id_produit) {
-		this.id_produit = id_produit;
+
+	public void set_produit(Produit produit) {
+		this.produit = produit;
 	}
-	public int getId_machine() {
-		return id_machine;
+
+	public Machine get_machine() {
+		return machine;
 	}
-	public void setId_machine(int id_machine) {
-		this.id_machine = id_machine;
+
+	public void set_machine(Machine machine) {
+		this.machine = machine;
 	}
+
 	public String getDuree() {
 		return duree;
 	}
+
 	public void setDuree(String duree) {
 		this.duree = duree;
 	}
+
 	public String getDateEntre() {
 		return dateEntre;
 	}
+
 	public void setDateEntre(String dateEntre) {
 		this.dateEntre = dateEntre;
 	}
+
 	public String getDateSortie() {
 		return dateSortie;
 	}
+
 	public void setDateSortie(String dateSortie) {
 		this.dateSortie = dateSortie;
 	}
-	
+
 	/* construtors */
 	public Log(String duree, String dateEntre) {
 		super();
 		this.duree = duree;
 		this.dateEntre = dateEntre;
+	}
+
+	/**
+	 * Insert a log
+	 * 
+	 * @param context
+	 */
+	public void insertLog(Context context) {
+		DatabaseSQLite data = new DatabaseSQLite(context,
+				Constantes.DATABASE_NAME, null, Constantes.DATABASE_VERSION);
+		SQLiteDatabase db = data.getWritableDatabase();
+
+		String[] COL = { Constantes.LOG_PRODUIT_ID, Constantes.LOG_MACHINE_ID,
+				Constantes.LOG_DUREE, Constantes.LOG_DATEENTREE,
+				Constantes.LOG_DATESORTIE };
+		String[] WHERE = {};
+
+		ContentValues content = new ContentValues();
+
+		content.put(Constantes.LOG_PRODUIT_ID, get_produit().getId_produit());
+		content.put(Constantes.LOG_MACHINE_ID, get_machine().getId_machine());
+		content.put(Constantes.LOG_DUREE, getDuree());
+		content.put(Constantes.LOG_DATEENTREE, getDateEntre());
+		content.put(Constantes.LOG_DATESORTIE, getDateSortie());
+
+		setId_log((int) db.insert(Constantes.TABLE_NAME_LOG, null, content));
+
+		db.close();
+	}
+
+	/**
+	 * Get a log using log_ID
+	 * 
+	 * @param context
+	 * @param ID
+	 */
+	public void getLogByID(Context context) {
+		DatabaseSQLite data = new DatabaseSQLite(context,
+				Constantes.DATABASE_NAME, null, Constantes.DATABASE_VERSION);
+		SQLiteDatabase db = data.getReadableDatabase();
+
+		Cursor monCu;
+
+		String[] COL = { Constantes.LOG_ID, Constantes.LOG_PRODUIT_ID,
+				Constantes.LOG_MACHINE_ID, Constantes.LOG_DUREE,
+				Constantes.LOG_DATEENTREE, Constantes.LOG_DATESORTIE };
+		String WHERE = Constantes.CLIENT_ID + " = ?";
+		String[] CLAUSE = { String.valueOf(getId_log()) };
+
+		monCu = db.query(Constantes.TABLE_NAME_LOG, COL, WHERE, CLAUSE,
+				null, null, null);
+		if (monCu.moveToFirst()) {
+			do {
+				set_produit(new Produit() monCu.getString(1));
+			} while (monCu.moveToNext());
+		}
+		db.close();
+	}
+
+	public void getClientNom(Context context) {
+		DatabaseSQLite data = new DatabaseSQLite(context,
+				Constantes.DATABASE_NAME, null, Constantes.DATABASE_VERSION);
+		SQLiteDatabase db = data.getReadableDatabase();
+
+		Cursor monCu;
+
+		String[] COL = { Constantes.CLIENT_ID, Constantes.CLIENT_NOM };
+		String WHERE = Constantes.CLIENT_NOM + " = ?";
+		String[] CLAUSE = { getNom() };
+
+		monCu = db.query(Constantes.TABLE_NAME_CLIENT, COL, WHERE, CLAUSE,
+				null, null, null);
+		if (monCu.moveToFirst()) {
+			do {
+				setId_client(monCu.getInt(0));
+			} while (monCu.moveToNext());
+		}
+		db.close();
+	}
+
+	public int upDateClient(Context context) {
+		DatabaseSQLite data = new DatabaseSQLite(context,
+				Constantes.DATABASE_NAME, null, Constantes.DATABASE_VERSION);
+		SQLiteDatabase db = data.getWritableDatabase();
+
+		int toReturn = 0;
+		String[] COL = { Constantes.CLIENT_ID, Constantes.CLIENT_NOM };
+		String[] WHERE = {};
+
+		ContentValues content = new ContentValues();
+
+		content.put(Constantes.CLIENT_ID, getId_client());
+		content.put(Constantes.CLIENT_NOM, getNom());
+
+		toReturn += db.update(Constantes.TABLE_NAME_CLIENT, content,
+				Constantes.CLIENT_ID + " = ?",
+				new String[] { String.valueOf(getId_client()) });
+
+		db.close();
+
+		return toReturn;
+	}
+
+	public void deletedClient(Context context) {
+		DatabaseSQLite data = new DatabaseSQLite(context,
+				Constantes.DATABASE_NAME, null, Constantes.DATABASE_VERSION);
+		SQLiteDatabase db = data.getReadableDatabase();
+
+		db.delete(Constantes.TABLE_NAME_CLIENT, Constantes.CLIENT_ID + " = ?",
+				new String[] { String.valueOf(getId_client()) });
+
+		db.close();
 	}
 }
--- MonProj/src/com/antoineanais/windowsgesture/Produit.java
+++ MonProj/src/com/antoineanais/windowsgesture/Produit.java
@@ -1,25 +1,34 @@
 package com.antoineanais.windowsgesture;
 
+import android.content.ContentValues;
+import android.content.Context;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
+
 public class Produit {
 
 	/* Members */
 	private int id_produit;
 	
-	private enum type{
+	private enum Type{
 		fenetre,
 		porte
 	};
-	private enum etat{
+	private enum Etat{
 		encours,
 		valide,
 		rebut,
 		standby
 	};
-	private enum materiel{
+	private enum Materiel{
 		aluminium,
 		acier,
 		acierrenforce
 	};
+	private String type;
+	private String etat;
+	private String materiel;
+	
 	private int avancement;
 	
 	/* Getters & Setters */
@@ -35,11 +44,152 @@ public class Produit {
 	public void setAvancement(int avancement) {
 		this.avancement = avancement;
 	}
+	public String getType() {
+		return type;
+	}
+	public void setType(String type) {
+		this.type = type;
+	}
+	public String getEtat() {
+		return etat;
+	}
+	public void setEtat(String etat) {
+		this.etat = etat;
+	}
+	public String getMateriel() {
+		return materiel;
+	}
+	public void setMateriel(String materiel) {
+		this.materiel = materiel;
+	}
 	
 	/* Construtors */
 	public Produit() {
 		super();
 		this.avancement = 0;
 	}
+	public Produit(int id_produit, Type type, Etat etat, Materiel materiel,
+			int avancement) {
+		super();
+		this.id_produit = id_produit;
+		this.type = type.toString();
+		this.etat = etat.toString();
+		this.materiel = materiel.toString();
+		this.avancement = avancement;
+	}
 	
+	/**
+	 * Insert un produit en base et set son id
+	 * @param context
+	 */
+	public void insertProduit(Context context) {
+		DatabaseSQLite data = new DatabaseSQLite(context,
+				Constantes.DATABASE_NAME, null, Constantes.DATABASE_VERSION);
+		SQLiteDatabase db = data.getWritableDatabase();
+
+		String[] COL = { Constantes.CLIENT_NOM };
+		String[] WHERE = {};
+
+		ContentValues content = new ContentValues();
+
+		content.put(Constantes.CLIENT_NOM, getNom());
+
+		setId_client((int)db.insert(Constantes.TABLE_NAME_CLIENT, null, content));
+		
+		db.close();
+	}
+
+	/**
+	 * Récupère le nom d'un client depuis son ID
+	 * @param context
+	 * @param ID
+	 */
+	public void getClientByID(Context context) {
+		DatabaseSQLite data = new DatabaseSQLite(context,
+				Constantes.DATABASE_NAME, null, Constantes.DATABASE_VERSION);
+		SQLiteDatabase db = data.getReadableDatabase();
+
+		Cursor monCu;
+
+		String[] COL = { Constantes.CLIENT_ID, Constantes.CLIENT_NOM };
+		String WHERE = Constantes.CLIENT_ID + " = ?";
+		String[] CLAUSE = { String.valueOf(getId_client()) };
+
+		monCu = db.query(Constantes.TABLE_NAME_CLIENT, COL, WHERE, CLAUSE, null,
+				null, null);
+		if (monCu.moveToFirst()) {
+			do {
+				setNom(monCu.getString(1));
+			} while (monCu.moveToNext());
+		}
+		db.close();
+	}
+
+	/**
+	 * Récupère l'ID d'un client en fonction de son nom
+	 * @param context
+	 */
+	public void getClientByNom(Context context) {
+		DatabaseSQLite data = new DatabaseSQLite(context,
+				Constantes.DATABASE_NAME, null, Constantes.DATABASE_VERSION);
+		SQLiteDatabase db = data.getReadableDatabase();
+
+		Cursor monCu;
+
+		String[] COL = { Constantes.CLIENT_ID, Constantes.CLIENT_NOM };
+		String WHERE = Constantes.CLIENT_NOM + " = ?";
+		String[] CLAUSE = { getNom() };
+
+		monCu = db.query(Constantes.TABLE_NAME_CLIENT, COL, WHERE, CLAUSE, null,
+				null, null);
+		if (monCu.moveToFirst()) {
+			do {
+				setId_client(monCu.getInt(0));
+			} while (monCu.moveToNext());
+		}
+		db.close();
+	}
+	
+	/**
+	 * Mise a jour des données du client
+	 * @param context
+	 * @return
+	 */
+	public int upDateClient(Context context) {
+		DatabaseSQLite data = new DatabaseSQLite(context,
+				Constantes.DATABASE_NAME, null, Constantes.DATABASE_VERSION);
+		SQLiteDatabase db = data.getWritableDatabase();
+
+		int toReturn = 0;
+		String[] COL = { Constantes.CLIENT_ID, Constantes.CLIENT_NOM };
+		String[] WHERE = {};
+
+		ContentValues content = new ContentValues();
+
+		content.put(Constantes.CLIENT_ID, getId_client());
+		content.put(Constantes.CLIENT_NOM, getNom());
+
+		toReturn += db.update(Constantes.TABLE_NAME_CLIENT, content,
+				Constantes.CLIENT_ID+ " = ?", 
+				new String[] { String.valueOf(getId_client()) });
+
+		db.close();
+
+		return toReturn;
+	}
+
+	/**
+	 * Suppression du client
+	 * @param context
+	 */
+	public void deletedClient(Context context) {
+		DatabaseSQLite data = new DatabaseSQLite(context,
+				Constantes.DATABASE_NAME, null, Constantes.DATABASE_VERSION);
+		SQLiteDatabase db = data.getReadableDatabase();
+
+		db.delete(Constantes.TABLE_NAME_CLIENT, Constantes.CLIENT_ID + " = ?",
+				new String[] { String.valueOf( getId_client()) });
+
+		db.close();
+	}
 }
